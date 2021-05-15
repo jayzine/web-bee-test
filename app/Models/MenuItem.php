947d80e616924e53242f76bda76cf15db6609ac7
jayzine <kumarjayant234@gmail.com>
2021-05-16 02:40:06 +0530

    Add files via upload

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    protected $table = 'menu_items';
 
+	protected $fillable = ['id', 'name', 'url', 'parent_id','created_at', 'updated_at'];
+
+    public function children()
+    {
+        return $this->hasMany(MenuItem::class, 'parent_id', 'id')->with('children');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(MenuItem::class, 'parent_id');
+    }
 }
