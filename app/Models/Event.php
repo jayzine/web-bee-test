fcda2fa118052ec17a1a34bff66814902139c817
jayzine <kumarjayant234@gmail.com>
2021-05-16 02:07:35 +0530

    Add files via upload

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Workshop;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+	protected $table = 'events';
+
+	protected $fillable = ['id', 'name', 'created_at', 'updated_at'];
+
+	public function workshops()
+	{
+		return $this->hasMany(Workshop::class);
+	}
 }
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -6,8 +6,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
+use App\Models\Event;
 
 class Workshop extends Model
 {
+	protected $table = 'workshops';
 
+	protected $fillable = ['id', 'start', 'end', 'event_id', 'name', 'created_at', 'updated_at'];
+
+	public function events()
+	{
+		return $this->belongsTo(Event::class);
+	}
 }
